6e21b9cf3b21f4a5d55ecd75862568e540f63802
Rahul <rahuln8897@gmail.com>
2023-08-23 19:59:13 +0530

    adding shipping

---
+++ shipping.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
+G="\e[32m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+yum install maven -y &>>$LOGFILE
+
+VALIDATE $? "Installing Maven"
+
+useradd roboshop &>>$LOGFILE
+
+mkdir /app &>>$LOGFILE
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>>$LOGFILE
+
+VALIDATE $? "Downloading shipping artifact"
+
+unzip /tmp/shipping.zip &>>$LOGFILE
+
+VALIDATE $? "Unzipping Shipping"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "Moving to app directory"
+
+mvn clean package &>>$LOGFILE
+
+VALIDATE $? "Packaging shipping app"
+
+mv target/shipping-1.0.jar shipping.jar &>>$LOGFILE
+
+VALIDATE $? "remaining shipping jar"
+
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>$LOGFILE
+
+VALIDATE $? "Copying shipping service"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon reload"
+
+systemctl enable shipping &>>$LOGFILE
+
+VALIDATE $? "Enabling shipping"
+
+systemctl start shipping &>>$LOGFILE
+
+VALIDATE $? "Starting shipping"
+
+yum install mysql -y &>>$LOGFILE
+
+VALIDATE $? "Installing mysql client"
+
+mysql -h mysql.joindevops.shop -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>$LOGFILE
+
+VALIDATE $? "Loaded countries and citites info"
+
+systemctl restart shipping &>>$LOGFILE
+
+VALIDATE $? "Restarting Shipping"
